05c9bb47649926bbacf7fa6cbff083fbe4877644
pseudonerd16 <sreejitaroytiu1923@gmail.com>
2020-11-20 23:52:20 +0530

    changes

--- jasc.js
+++ jasc.js
@@ -3,54 +3,54 @@ let tempIcon = document.getElementById("weather-icon");
 let tempValue = document.getElementById("temperature-degree");
 let climate = document.getElementById("temperature-description");
 let iconFile;
+let degreeSpan = document.querySelector(".degree-section span");
+// console.log(loc, tempIcon, tempValue, climate)
+window.addEventListener('load', () => {
+  let long;
+  let lat;
 
-window.addEventListener('load', ()=> {
-    let long;
-    let lat;
-
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
-            const proxy = "https://cors-anywhere.herokuapp.com/";
-            const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=1a30f089ccc08e40204fc421d9fa87e4`;
-            fetch(api)
-              .then((response) => {
-                  return response.json();
-              })
-              .then(data => {
-                  const {name} = data;
-                  const {feels_like} = data.main;
-                  const {id, main} = data.weather[0];
-                  loc.textContent = name;
-                  climate.textContent = main;
-                  tempValue.textContent = Math.round(feels_like-273);
-                  if (id < 250) {
-                      tempIcon.src = "./icons/storm.svg"
-                  }
-                  if (id < 350) {
-                    tempIcon.src = "./icons/drizzle.svg"
-                }
-                  if (id < 550) {
-                    tempIcon.src = "./icons/rain.svg"
-                }
-                  if (id < 650) {
-                    tempIcon.src = "./icons/snow.svg"
-                }
-                  if (id < 800) {
-                    tempIcon.src = "./icons/atmosphere.svg"
-                }
-                  if (id === 800) {
-                    tempIcon.src = "./icons/clear.svg"
-                }
-                  if (id > 800) {
-                    tempIcon.src = "./icons/clouds.svg"
-                }
-
-                  console.log(data);
-              })
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      // console.log(lat,long);
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=1a30f089ccc08e40204fc421d9fa87e4`;
+      fetch(api)
+        .then((response) => {
+          return response.json();
         })
-    }
+        .then(data => {
+          console.log(data);
+          const { name } = data;
+          const { feels_like } = data.main;
+          const { id, main, icon } = data.weather[0];
+          celsius = Math.floor(feels_like - 273);
+          fahrenheit = Math.floor((celsius * (9 / 5)) + 32);
+          loc.innerText = name;
+
+          climate.innerText = main;
+          tempValue.innerText = `${celsius}°`;
+
+          tempIcon.src = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+          tempValue.addEventListener("click", () => {
+            // console.log(degreeSpan.innerText)
+            if (degreeSpan.innerText === "C") {
+              degreeSpan.innerText = "F"
+              tempValue.innerText = `${fahrenheit}°`;
+            }
+            else {
+              degreeSpan.innerText = "C"
+              tempValue.innerText = `${celsius}°`;
+
+
+            }
+          })
+          // console.log(degreeSpan.innerText==="C");
+        })
+    })
+  }
 
 });
 
